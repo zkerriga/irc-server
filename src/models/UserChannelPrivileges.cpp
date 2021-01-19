bdd10d967935f1ba3808cd14858aeb54b4914efc
zkerriga <daniil.63@yandex.ru>
2021-01-19 14:32:16 +0300

    Add Concreate Mods for Channels and Users

---
+++ src/models/UserChannelPrivileges.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   UserChannelPrivileges.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 14:30:25 by zkerriga          #+#    #+#             */
+/*   Updated: 2021/01/19 14:30:26 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "UserChannelPrivileges.hpp"
+
+UserChannelPrivileges::UserChannelPrivileges() {
+	/* todo: default constructor */
+}
+
+UserChannelPrivileges::UserChannelPrivileges(const UserChannelPrivileges & other) {
+	/* todo: copy constructor */
+	*this = other;
+}
+
+UserChannelPrivileges::~UserChannelPrivileges() {
+	/* todo: destructor */
+}
+
+UserChannelPrivileges & UserChannelPrivileges::operator=(const UserChannelPrivileges & other) {
+	if (this != &other) {
+		/* todo: operator= */
+	}
+	return *this;
+}
+
---
+++ src/models/UserChannelPrivileges.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   UserChannelPrivileges.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 14:30:18 by zkerriga          #+#    #+#             */
+/*   Updated: 2021/01/19 14:30:19 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class UserChannelPrivileges {
+public:
+	UserChannelPrivileges();
+	UserChannelPrivileges(const UserChannelPrivileges & other);
+	~UserChannelPrivileges();
+	UserChannelPrivileges & operator= (const UserChannelPrivileges & other);
+
+private:
+
+};
+
