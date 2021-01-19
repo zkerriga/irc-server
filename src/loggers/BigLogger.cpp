fba82b9b6cc4f297e871bf173c59e1945356cdb8
zkerriga <daniil.63@yandex.ru>
2021-01-19 10:22:02 +0300

    Add basic files

---
+++ src/loggers/BigLogger.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BigLogger.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 09:58:34 by zkerriga          #+#    #+#             */
+/*   Updated: 2021/01/19 09:58:43 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BigLogger.hpp"
+
+BigLogger::BigLogger() {
+	/* todo: default constructor */
+}
+
+BigLogger::BigLogger(const BigLogger & other) {
+	/* todo: copy constructor */
+}
+
+BigLogger::~BigLogger() {
+	/* todo: destructor */
+}
+
+BigLogger & BigLogger::operator=(const BigLogger & other) {
+	if (this != &other) {
+		/* todo: operator= */
+	}
+	return *this;
+}
---
+++ src/loggers/BigLogger.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BigLogger.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 09:58:33 by zkerriga          #+#    #+#             */
+/*   Updated: 2021/01/19 09:58:43 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class BigLogger {
+public:
+	BigLogger();
+	BigLogger(const BigLogger & other);
+	~BigLogger();
+	BigLogger & operator= (const BigLogger & other);
+
+private:
+
+};
