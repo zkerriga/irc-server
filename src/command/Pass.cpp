2bccfb06e0a0443bf5021a6a904d2547c839d38c
cgarth <cgarth@si-i6.kzn.21-school.ru>
2021-01-22 16:47:09 +0300

    good version ' :'

--- src/command/Pass.cpp
+++ src/command/Pass.cpp
@@ -40,72 +40,9 @@ ACommand *Pass::create(const std::string & commandLine, const int senderFd) {
 	return new Pass(commandLine, senderFd);
 }
 
-bool isThisVersion(const std::string & str)
-{
-    std::string tmp = str;
-
-    if (str.size() < 4)
-        return false;
-    tmp=str.substr(0, 4);
-
-    if (str.size() >=4 && str.size() <= 14 && tmp.find_first_not_of("0123456789") == std::string::npos && str.find_first_of("|") == std::string::npos)
-        return true;
-    return false;
-}
-
-bool isThisFlag(std::string str)
-{
-    size_t pos;
-    std::string tmp = str;
-    std::string first;
-    std::string second;
-
-    if ((pos = tmp.find_first_of("|") != std::string::npos) && tmp.size() <= 100){
-        first = tmp.substr(0,pos);
-        if (first.size() == 0)
-            first = "IRC";
-        tmp.erase(0,pos);
-        second = tmp.substr(0, tmp.size());
-        return true;
-    }
-    return false;
-}
-
-bool isThisOption(std::string str)
-{
-    if str
-}
-
 void Pass::_validateParams(IServerForCmd & server) {
 	/*todo: validate params*/
-    receivers_type	receivers;
-    reply_args_type	reply;
-	std::list<std::string> args = Parser::splitArgs(_rawCmd);
-    std::list<std::string>::iterator itb = args.begin();
-    std::list<std::string>::iterator ite = args.end();
-    std::string tmp="";
 
-    receivers.push_back(_senderFd);
-
-    while (strcmp("PASS", *itb) != 0 && itb != ite)
-        ++itb;
-
-    ++itb;
-    // Проверяем что есть пароль
-    if (itb != ite)
-        _password = *itb;
-
-    ++itb;
-    // Проверяем что есть версия
-    if (itb != ite && isThisVersion(*itb)){
-        _version = *(itb);
-    }
-
-
-
-    if (args.size() != 2)
-    //if (strcmp("PASS", *itb) != 0)
-	    _reply(receivers, 461, reply);
 	(void)server;
 }
 
