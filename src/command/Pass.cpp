00ef93f2e5765f23f6da812ef61242f1d4d4cfbb
zkerriga <daniil.63@yandex.ru>
2021-01-25 20:48:03 +0300

    Pass fixed and refactored

--- src/command/Pass.cpp
+++ src/command/Pass.cpp
@@ -12,72 +12,60 @@
 
 #include "Pass.hpp"
 
-Pass::Pass() : ACommand("nouse", 0) {
-	/* todo: default constructor */
-}
-
-Pass::Pass(const Pass & other) : ACommand("nouse", 0)  {
-	/* todo: copy constructor */
+Pass::Pass() : ACommand("", 0) {}
+Pass::Pass(const Pass & other) : ACommand("", 0) {
 	*this = other;
+}
+Pass & Pass::operator=(const Pass & other) {
+	if (this != &other) {}
+	return *this;
 }
 
 Pass::~Pass() {
 	/* todo: destructor */
 }
 
-Pass & Pass::operator=(const Pass & other) {
-	if (this != &other) {
-		/* todo: operator= */
-	}
-	return *this;
-}
-
-Pass::Pass(const std::string & rawCmd, const int senderFd)
+Pass::Pass(const std::string & rawCmd, const socket_type senderFd)
 	: ACommand(rawCmd, senderFd) {}
 
-ACommand *Pass::create(const std::string & commandLine, const int senderFd) {
+ACommand *Pass::create(const std::string & commandLine, const socket_type senderFd) {
 	return new Pass(commandLine, senderFd);
 }
 
-const char *		Pass::commandName = "PASS";
-#include <algorithm>
+const char *	Pass::commandName = "PASS";
 
-bool isThisVersion(const std::string & str)
-{
-    std::string tmp = str;
-
-    if (str.size() < 4)
-        return false;
-    tmp=str.substr(0, 4);
-
-    if (str.size() >=4 && str.size() <= 14
-    	&& tmp.find_first_not_of("0123456789") == std::string::npos)
-        return true;
-    return false;
+bool isThisVersion(const std::string & str) {
+	if (str.size() < 4) {
+		return false;
+	}
+	const std::string	tmp = str.substr(0, 4);
+	return (str.size() >= 4 && str.size() <= 14
+			&& tmp.find_first_not_of("0123456789") == std::string::npos);
 }
 
-bool isThisFlag(const std::string & str)
-{
-    size_t pos = 0;
-    std::string tmp = str;
-    std::string first;
-    std::string second;
+bool isThisFlag(const std::string & str) {
+	size_t			pos = 0;
+	std::string		tmp = str;
+	std::string		first;
+	std::string		second;
 
-    if ((pos = tmp.find_first_of('|')) != tmp.find_last_of('|') || tmp.find_last_of('|') == std::string::npos || tmp.size() > 100 )
-        return false;
-    first = tmp.substr(0,pos);
-    tmp.erase(0,pos);
-    second = tmp.substr(0, tmp.size());
-    return true;
+	if ((pos = tmp.find_first_of('|')) != tmp.find_last_of('|')
+		|| tmp.find_last_of('|') == std::string::npos || tmp.size() > 100)
+	{
+		return false;
+	}
+	first = tmp.substr(0, pos);
+	tmp.erase(0, pos);
+	second = tmp.substr(0, tmp.size()); /* todo: бесполезные операции? */
+	return true;
 }
 
-bool isThisOption(const std::string & str)
-{
+bool isThisOption(const std::string & str) {
 	if (str.length() > 2) {
 		return false;
 	}
 	if (str.length() == 1) {
-		if (str[0] != 'P' || str[1] != 'Z') {
+		if (str[0] != 'P' && str[1] != 'Z') {
 			return false;
 		}
 	}
