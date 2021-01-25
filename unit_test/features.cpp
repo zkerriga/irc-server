95b10f9713009e058948d26dc7e3ff864f42fb98
zkerriga <daniil.63@yandex.ru>
2021-01-25 18:40:23 +0300

    The test fixed

--- unit_test/features.cpp
+++ unit_test/features.cpp
@@ -3,40 +3,6 @@
 #include "ACommand.hpp"
 #include "Parser.hpp"
 
-
-TEST(test, test_of_test) {
-	SUCCEED();
-}
-
-/*
-TEST(getNameOfCmd, find) {
-	static const char *		crlf = "\r\n";
-
-	ASSERT_EQ(getCommandNameByLine(""), "");
-	ASSERT_EQ(getCommandNameByLine("COMMAND"), "");
-	ASSERT_EQ(getCommandNameByLine("     COMMAND\r\n"), "");
-
-	ASSERT_EQ(getCommandNameByLine("COMMAND\r\n"), "COMMAND");
-	ASSERT_EQ(getCommandNameByLine(":prefix COMMAND\r\n"), "COMMAND");
-	ASSERT_EQ(getCommandNameByLine(":prefix      COMMAND\r\n"), "COMMAND");
-	ASSERT_EQ(getCommandNameByLine(":prefix      COMMAND    arg\r\n"), "COMMAND");
-	ASSERT_EQ(getCommandNameByLine(":prefix      123    arg\r\n"), "123");
-	ASSERT_EQ(getCommandNameByLine(":prefix      command    arg\r\n"), "command");
-}
-
-TEST(getCommand, find) {
-	ACommand *		ret;
-
-	ASSERT_EQ(getCommandObjectByName(""), nullptr);
-	ASSERT_EQ(getCommandObjectByName("asdasda"), nullptr);
-	ASSERT_EQ(getCommandObjectByName("\n"), nullptr);
-
-	ASSERT_TRUE(getCommandObjectByName("pass") != nullptr);
-	ASSERT_TRUE(getCommandObjectByName("paSs") != nullptr);
-	ASSERT_TRUE(getCommandObjectByName("PASS") != nullptr);
-}
-*/
-
 TEST(parser, get_command) {
 	Parser::receive_container	receiveBuffers;
 	receiveBuffers[3] = std::string("PASS password") + Parser::crlf;
@@ -95,51 +61,50 @@ TEST(parser, cutstring1) {
 }
 
 TEST(parserSplit, testsplit) {
-    std::string             input(":prefix!pr@pr2 Command Arg1 Arg2 :Arg3 Arg4 :Arg5\r\n");
-    Parser::arguments_array expect;
-    expect.push_back(":prefix!pr@pr2");
-    expect.push_back("Command");
-    expect.push_back("Arg1");
-    expect.push_back("Arg2");
-    expect.push_back(":Arg3 Arg4 :Arg5");
-    ASSERT_EQ(expect, Parser::splitArgs(input));
+	std::string				input(":prefix!pr@pr2 Command Arg1 Arg2 :Arg3 Arg4 :Arg5\r\n");
+	Parser::arguments_array	expect;
+	expect.push_back(":prefix!pr@pr2");
+	expect.push_back("Command");
+	expect.push_back("Arg1");
+	expect.push_back("Arg2");
+	expect.push_back(":Arg3 Arg4 :Arg5");
+	ASSERT_EQ(expect, Parser::splitArgs(input));
 
-    std::string             input2(":prefix!pr@pr2 Command Arg1 Arg2 Arg3\r\n");
-    Parser::arguments_array  expect2;
-    expect2.push_back(":prefix!pr@pr2");
-    expect2.push_back("Command");
-    expect2.push_back("Arg1");
-    expect2.push_back("Arg2");
-    expect2.push_back("Arg3");
-    ASSERT_EQ(expect2, Parser::splitArgs(input2));
+	std::string				input2(":prefix!pr@pr2 Command Arg1 Arg2 Arg3\r\n");
+	Parser::arguments_array	expect2;
+	expect2.push_back(":prefix!pr@pr2");
+	expect2.push_back("Command");
+	expect2.push_back("Arg1");
+	expect2.push_back("Arg2");
+	expect2.push_back("Arg3");
+	ASSERT_EQ(expect2, Parser::splitArgs(input2));
 
-    std::string             input3("Command Arg1 Arg2 Arg3\r\n");
-    Parser::arguments_array  expect3;
-    expect3.push_back("Command");
-    expect3.push_back("Arg1");
-    expect3.push_back("Arg2");
-    expect3.push_back("Arg3");
-    ASSERT_EQ(expect3, Parser::splitArgs(input3));
+	std::string				input3("Command Arg1 Arg2 Arg3\r\n");
+	Parser::arguments_array	expect3;
+	expect3.push_back("Command");
+	expect3.push_back("Arg1");
+	expect3.push_back("Arg2");
+	expect3.push_back("Arg3");
+	ASSERT_EQ(expect3, Parser::splitArgs(input3));
 
-    std::string             input4("\r\n");
-    Parser::arguments_array  expect4;
-    ASSERT_EQ(expect4, Parser::splitArgs(input4));
+	std::string				input4("\r\n");
+	Parser::arguments_array	expect4;
+	ASSERT_EQ(expect4, Parser::splitArgs(input4));
 
 
-    std::string             input5(":prefix!pr@pr2 Command Arg1 Arg2 Arg3:Arg4 Arg5 :Arg6 :Arg7\r\n");
-    Parser::arguments_array  expect5;
-    expect5.push_back(":prefix!pr@pr2");
-    expect5.push_back("Command");
-    expect5.push_back("Arg1");
-    expect5.push_back("Arg2");
-    expect5.push_back("Arg3:Arg4");
-    expect5.push_back("Arg5");
-    expect5.push_back(":Arg6 :Arg7");
-    ASSERT_EQ(expect5, Parser::splitArgs(input5));
+	std::string				input5(":prefix!pr@pr2 Command Arg1 Arg2 Arg3:Arg4 Arg5 :Arg6 :Arg7\r\n");
+	Parser::arguments_array	expect5;
+	expect5.push_back(":prefix!pr@pr2");
+	expect5.push_back("Command");
+	expect5.push_back("Arg1");
+	expect5.push_back("Arg2");
+	expect5.push_back("Arg3:Arg4");
+	expect5.push_back("Arg5");
+	expect5.push_back(":Arg6 :Arg7");
+	ASSERT_EQ(expect5, Parser::splitArgs(input5));
 
-	std::string             input6(":irc.exampl e.net PASS  0210-IRC+ ngIRCd|26.1:CHLMSXZ PZ\r\n");
-	/* todo: split string by " :" */
-	Parser::arguments_array  expect6;
+	std::string				input6(":irc.example.net PASS  0210-IRC+ ngIRCd|26.1:CHLMSXZ PZ\r\n");
+	Parser::arguments_array	expect6;
 	expect6.push_back(":irc.example.net");
 	expect6.push_back("PASS");
 	expect6.push_back("0210-IRC+");
