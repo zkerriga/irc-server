fd0eb68b84f1fd0db984b41f07c530648e3f5c33
Matrus <matrus@student.21-school.ru>
2021-01-29 18:49:39 +0300

    add todos to pinging

--- src/Server.cpp
+++ src/Server.cpp
@@ -355,17 +355,20 @@ void Server::_closeConnections(std::set<socket_type> & connections) {
 
 	for (; it != ite; ++it) {
 		if ((requestFound = tools::find(_requests, *it, tools::compareBySocket)) != nullptr) { // RequestForConnect
+			/* todo: use _deleteRequest() instead */
 			_requests.remove(requestFound);
 			BigLogger::cout(std::string("Request on fd ") + requestFound->getSocket() + " removed.");
 			delete requestFound;
 		}
 		else if ((clientFound = tools::find(_clients, *it, tools::compareBySocket)) != nullptr) {
+			/* todo: use _deleteClient() instead */
 			/* todo: send "QUIT user" to other servers */
 			_clients.remove(clientFound);
 			BigLogger::cout(std::string("Client on fd ") + clientFound->getSocket() + " removed.");
 			delete clientFound;
 		}
 		else if ((serverFound = tools::find(_servers, *it, tools::compareBySocket)) != nullptr) {
+			/* todo: use _deleteServer() instead */
 			/* todo: send "SQUIT server" to other servers */
 			/* todo: send "QUIT user" (for disconnected users) to other servers */
 			_servers.remove(serverFound);
