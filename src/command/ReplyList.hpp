8b7cccba19189ac28abe0a22a348c2a4b2369948
Matrus <matrus@student.21-school.ru>
2021-01-30 11:26:44 +0300

    add errPasswdMismatch()'

--- src/command/ReplyList.hpp
+++ src/command/ReplyList.hpp
@@ -22,6 +22,7 @@ std::string errNeedMoreParams(const std::string & commandName);
 std::string errAlreadyRegistered();
 std::string errNoSuchServer(const std::string & serverName);
 std::string errNoOrigin();
+std::string errPasswdMismatch();
 
 std::string sendPong(const std::string & destination, const std::string & origin);
 //std::string sendPing(const std::string & destination, const std::string & origin);
