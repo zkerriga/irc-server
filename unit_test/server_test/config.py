831be827bef3c10cf92893d768a37fb10b900c06
zkerriga <daniil.63@yandex.ru>
2021-01-30 16:27:50 +0300

    Prepase Improve tests

---
+++ unit_test/server_test/config.py
@@ -0,0 +1,20 @@
+from typing import Final
+
+NGIRCD_SERVER_NAME: Final[str] = "irc.example.net"
+OUR_SERVER_NAME: Final[str] = "zkerriga.matrus.cgarth.com"
+
+SERVER_IP: Final[str] = "localhost"
+SERVER_PORT: Final[str] = "6668"
+
+CONF_PASSWORD: Final[str] = "pass"
+CONF_SERVER_INFO: Final[str] = "It's another great day!"
+CONF_SERVER_NAME: Final[str] = "test.net"
+
+PASS_PARAMS: Final[str] = "0210-IRC+ ngIRCd| P"
+
+CR_LF: Final[str] = "\r\n"
+
+NOTHING: Final[str] = "[NOTHING]"
+
+BINARY_SERVER_PATH: Final[str] = "../../ircserv"
+BINARY_SERVER_LOG_PATH: Final[str] = "server-log.txt"
