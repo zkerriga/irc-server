ab05659f9081606c4823e1a89526c6a2e383aeea
zkerriga <daniil.63@yandex.ru>
2021-02-05 12:28:31 +0300

    Start move tests

---
+++ server_test/pass_test.py
@@ -0,0 +1,27 @@
+from config import *
+from test_class import Test
+
+
+def wrong_count_params_461() -> Test:
+	"""
+	in RFC "PASS :Not enough parameters"
+	"""
+	return Test(
+		test_name="461 ERR_NEEDMOREPARAMS",
+		commands=[
+			"PASS",
+			"PASS 1 2",
+			"PASS 1 :123",
+			"pass",
+			":badprefix PASS",
+			":badprefix PASS 1 2"
+		],
+		expected=[
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters",
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters",
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters",
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters",
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters",
+			f":{OUR_SERVER_NAME} 461 PASS :Not enough parameters"
+		]
+	)
