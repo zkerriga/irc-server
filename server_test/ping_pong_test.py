36f4ca4781719e871e69e61b31d683093a212315
zkerriga <daniil.63@yandex.ru>
2021-02-05 12:54:53 +0300

    Move ping-pong-tests

--- server_test/ping_pong_test.py
+++ server_test/ping_pong_test.py
@@ -33,3 +33,61 @@ def ping_pong() -> Test:
 			f":{OUR_SERVER_NAME} PONG {CONF_SERVER_NAME} {CONF_SERVER_NAME}"
 		]
 	)
+
+
+def ping_after_good_registration_409_ERR_NOORIGIN() -> Test:
+	return Test(
+		test_name="server ping_error 409 ERR_NOORIGIN",
+		commands=CONNECT_COMMANDS + [
+			"PING",
+			f":{CONF_SERVER_NAME} PING"
+		],
+		expected=CONNECT_EXPECTED + [
+			f":{OUR_SERVER_NAME} 409 :No origin specified",
+			f":{OUR_SERVER_NAME} 409 :No origin specified"
+		]
+	)
+
+
+def ping_after_good_registration_402_ERR_NOSUCHSERVER() -> Test:
+	return Test(
+		test_name="server ping_error 402 ERR_NOSUCHSERVER",
+		commands=CONNECT_COMMANDS + [
+			"PING trash1 trash2",
+			f":{CONF_SERVER_NAME} PING trash1 trash2"
+		],
+		expected=CONNECT_EXPECTED + [
+			f":{OUR_SERVER_NAME} 402 trash2 :No such server",
+			f":{OUR_SERVER_NAME} 402 trash2 :No such server"
+		]
+	)
+
+
+def ping_local_connect_ignoring() -> Test:
+	return Test(
+		test_name="ignoring command/ bad prefix",
+		commands=CONNECT_COMMANDS + [
+			":bad_prefix PING"
+		],
+		expected=CONNECT_EXPECTED + [
+			NOTHING
+		]
+	)
+
+
+def pong_after_good_registration() -> Test:
+	return Test(
+		test_name="good pong",
+		commands=CONNECT_COMMANDS + [
+			f":{CONF_SERVER_NAME} PONG {CONF_SERVER_NAME} {CONF_SERVER_NAME}",
+			f":{CONF_SERVER_NAME} PONG trash {CONF_SERVER_NAME}",
+			f":badprefix pong {CONF_SERVER_NAME} {CONF_SERVER_NAME}",
+			f":{CONF_SERVER_NAME} PONG {OUR_SERVER_NAME} {CONF_SERVER_NAME}"
+		],
+		expected=CONNECT_EXPECTED + [
+			f":{CONF_SERVER_NAME} PONG {CONF_SERVER_NAME} {CONF_SERVER_NAME}",
+			f":{OUR_SERVER_NAME} 402 trash :No such server",
+			NOTHING,
+			NOTHING
+		]
+	)
