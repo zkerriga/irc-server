2721b6ad345f99229aa7113d89185ed0bc68147d
cgarth <cgarth@er-d4.kzn.21-school.ru>
2021-02-09 18:08:45 +0300

    2

--- src/command/Squit.hpp
+++ src/command/Squit.hpp
@@ -41,7 +41,7 @@ private:
     bool		_isPrivelegeValid(const IServerForCmd & server, char flag);
     bool		_isParamsValid(const IServerForCmd & server);
     void        _createAllReply(IServerForCmd & server);
-    void        _closeAllConnection(IServerForCmd & server);
+//    void        _closeAllConnection(IServerForCmd & server);
     void		_execute(IServerForCmd & server);
 
     std::string		_server;
