63ff7cdf210b4f57c2adff9f4ad538d8e674fdcd
zkerriga <daniil.63@yandex.ru>
2021-02-10 20:12:48 +0300

    Clean and add getInfo;

--- src/Server.cpp
+++ src/Server.cpp
@@ -16,9 +16,10 @@
 #include "Ping.hpp"
 #include "ServerCmd.hpp"
 
-Server::Server() : c_pingConnectionsTimeout(), c_maxMessageLen(), c_serverName(), c_conf() {}
+Server::Server() : c_tryToConnectTimeout(), c_pingConnectionsTimeout(), c_maxMessageLen(), c_serverName(), c_conf() {}
 Server::Server(const Server & other)
-		: c_pingConnectionsTimeout(other.c_pingConnectionsTimeout),
+		: c_tryToConnectTimeout(),
+		  c_pingConnectionsTimeout(other.c_pingConnectionsTimeout),
 		  c_maxMessageLen(other.c_maxMessageLen),
 		  c_serverName(other.c_serverName), c_conf(other.c_conf)
 {
@@ -38,10 +39,11 @@ static std::string _connectionToPrint(const Configuration::s_connection * conn) 
 }
 
 Server::Server(const Configuration & conf)
-	: c_pingConnectionsTimeout(conf.getPingConnectionTimeout()),
+	: c_tryToConnectTimeout(conf.getRequestTimeout()),
+	  c_pingConnectionsTimeout(conf.getPingConnectionTimeout()),
 	  c_maxMessageLen(conf.getMaxMessageLength()),
 	  c_serverName(conf.getServerName()), c_conf(conf),
-	  _serverInfo(":It's another great day!")
+	  _serverInfo(conf.getServerInfo())
 {
 	BigLogger::cout(std::string("Create server with:\n\tport = ") + \
 		c_conf.getPort() + "\n\tpassword = " + c_conf.getPassword() +\
