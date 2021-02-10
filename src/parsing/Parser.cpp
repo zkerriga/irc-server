c869bd9c54ebd89a03836b73db5836c657aff7b4
Matrus <matrus@student.21-school.ru>
2021-02-10 14:34:50 +0300

    add nick to parser

--- src/parsing/Parser.cpp
+++ src/parsing/Parser.cpp
@@ -34,6 +34,7 @@ const Parser::pair_name_construct	Parser::all[] = {
 		{.commandName=Pong::commandName, .create=Pong::create},
 		{.commandName=ServerCmd::commandName, .create=ServerCmd::create},
         {.commandName=Squit::commandName, .create=Squit::create},
+		{.commandName=Nick::commandName, .create=Nick::create},
 		{.commandName=nullptr,		.create=nullptr}
 };
 
