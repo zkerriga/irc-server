7361f92b76e712729733c01cdc04a62b89217397
zkerriga <daniil.63@yandex.ru>
2021-02-11 20:16:02 +0300

    Prepare JOIN

--- src/command/Join.cpp
+++ src/command/Join.cpp
@@ -11,23 +11,54 @@
 /* ************************************************************************** */
 
 #include "Join.hpp"
+#include "BigLogger.hpp"
+#include "Parser.hpp"
 
-Join::Join() {
-	/* todo: default constructor */
-}
-
-Join::Join(const Join & other) {
-	/* todo: copy constructor */
+Join::Join() : ACommand("", 0) {}
+Join::Join(const Join & other) : ACommand("", 0) {
 	*this = other;
 }
+Join & Join::operator=(const Join & other) {
+	if (this != &other) {}
+	return *this;
+}
+
 
 Join::~Join() {
 	/* todo: destructor */
 }
 
-Join & Join::operator=(const Join & other) {
-	if (this != &other) {
-		/* todo: operator= */
+Join::Join(const std::string & rawCmd, const socket_type senderSocket)
+	: ACommand(rawCmd, senderSocket) {}
+
+ACommand * Join::create(const std::string & commandLine, const socket_type senderSocket) {
+	return new Join(commandLine, senderSocket);
+}
+
+const char * const	Join::commandName = "JOIN";
+
+ACommand::replies_container Join::execute(IServerForCmd & server) {
+	BigLogger::cout(std::string(commandName) + ": execute");
+	if (_isParamsValid(server)) {
+		_execute(server);
 	}
-	return *this;
+	return _commandsToSend;
+}
+
+
+
+bool Join::_isParamsValid(const IServerForCmd & server) {
+	const Parser::arguments_array			arguments	= Parser::splitArgs(_rawCmd);
+	Parser::arguments_array::const_iterator	it			= arguments.begin();
+	Parser::arguments_array::const_iterator	ite			= arguments.end();
+	static const int						numberOfArguments = 4;
+
+	if (Parser::isPrefix(*it))
+
+	/* todo */
+	return false;
+}
+
+void Join::_execute(IServerForCmd & server) {
+	/* todo: exec */
 }
