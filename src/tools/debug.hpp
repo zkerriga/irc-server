6d54239a31c7338732227b9e3b6ddfc795ac0b9c
zkerriga <daniil.63@yandex.ru>
2021-02-13 21:53:47 +0300

    Add DEBUG_LVL

---
+++ src/tools/debug.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/12 22:59:42 by zkerriga          #+#    #+#             */
+/*   Updated: 2021/02/13 22:59:42 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#define DEBUG_LVL 1
+
+#if DEBUG_LVL > 0
+#define DEBUG1(exp) { exp }
+#else
+#define DEBUG1(exp) {     }
+#endif
+
+#if DEBUG_LVL > 1
+#define DEBUG2(exp) { exp }
+#else
+#define DEBUG2(exp) {     }
+#endif
+
+#if DEBUG_LVL > 2
+#define DEBUG3(exp) { exp }
+#else
+#define DEBUG3(exp) {     }
+#endif
