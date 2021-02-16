80077c7297f22120455e3c1d5e3d612abc5315ca
Matrus <matrus@student.21-school.ru>
2021-02-16 17:36:40 +0300

    setting modes done

--- src/command/Mode.hpp
+++ src/command/Mode.hpp
@@ -37,9 +37,21 @@ public:
 private:
 
 	enum setModesErrors {
+		FAIL_CRITICAL,
+		FAIL,
 		SUCCESS,
-		NOTONCHANNEL
+		NOTONCHANNEL,
+		UNKNOWNMODE
 	};
+
+	template<class objType>
+	struct map_mode_fuction {
+		char			mode;
+		setModesErrors	(Mode::*modeSetter)(const IServerForCmd & server, objType client, bool isSet);
+	};
+
+	static const map_mode_fuction<IClient *> _mapModeSetClient[];
+	static const map_mode_fuction<IChannel *> _mapModeSetChannel[];
 
 	static const char set;
 	static const char del;
@@ -50,7 +62,6 @@ private:
 	void	_executeForChannel(IServerForCmd & server, const IChannel * channel);
 	void	_executeForClient(IServerForCmd & server, IClient * client);
 
-	setModesErrors _trySetModesToClient(IServerForCmd & server, IClient * client);
 	void		_createAllReply(const IServerForCmd & server, const std::string & reply);
 
 	std::string _targetChannelOrNickname;
@@ -61,12 +72,20 @@ private:
 	std::string _params[c_modeMaxParams];
 
 	static const Parser::parsing_unit_type<Mode> _parsers[];
-
 	Parser::parsing_result_type _prefixParser(const IServerForCmd & server, const std::string & prefixArg);
 	Parser::parsing_result_type _commandNameParser(const IServerForCmd & server, const std::string & prefixArg);
 	Parser::parsing_result_type _targetParser(const IServerForCmd & server, const std::string & targetArg);
 	Parser::parsing_result_type _modesParser(const IServerForCmd & server, const std::string & modesArg);
 	Parser::parsing_result_type _paramParser(const IServerForCmd & server, const std::string & paramArg);
+
+	Mode::setModesErrors
+	_trySetModesToClient(const IServerForCmd & server, IClient * client,
+						 std::string::size_type & pos);
+	setModesErrors _trySetClient_a(const IServerForCmd & serer, IClient * client, bool isSet);
+	setModesErrors _trySetClient_i(const IServerForCmd & serer, IClient * client, bool isSet);
+	setModesErrors _trySetClient_w(const IServerForCmd & serer, IClient * client, bool isSet);
+	setModesErrors _trySetClient_r(const IServerForCmd & serer, IClient * client, bool isSet);
+	setModesErrors _trySetClient_o(const IServerForCmd & serer, IClient * client, bool isSet);
 
 	Mode();
 	Mode(const Mode & other);
