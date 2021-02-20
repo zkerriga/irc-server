6d9566e92e0ec68635388d59ca631ffcc68c7a17
zkerriga <daniil.63@yandex.ru>
2021-02-20 10:30:36 +0300

    Add _defaultPrefixParser

--- src/interfaces/ACommand.cpp
+++ src/interfaces/ACommand.cpp
@@ -60,6 +60,34 @@ void ACommand::_broadcastToServers(const IServerForCmd & server,
 	}
 }
 
+Parser::parsing_result_type
+ACommand::_defaultPrefixParser(const IServerForCmd & server, const std::string & prefixArgument) {
+	/**
+	 * \info
+	 * A parser for registered connections.
+	 * Checks the incoming prefix and fills in _prefix.
+	 *
+	 * \return
+	 * SUCCESS - if there is a prefix and it is processed correctly
+	 * SKIP_ARGUMENT - there is no prefix, but the command came from the client
+	 * CRITICAL_ERROR - if the prefix cannot be processed correctly
+	 */
+	const IClient *	nearestClient = server.findNearestClientBySocket(_senderFd);
+	if (nearestClient) {
+		_prefix.name = nearestClient->getName();
+		_prefix.host = nearestClient->getHost();
+		_prefix.user = nearestClient->getUsername();
+		return Parser::isPrefix(prefixArgument) ? Parser::SUCCESS : Parser::SKIP_ARGUMENT;
+	}
+	else if (server.findNearestServerBySocket(_senderFd)) {
+		if (Parser::isPrefix(prefixArgument)) {
+			Parser::fillPrefix(_prefix, prefixArgument);
+			return server.findServerByName(_prefix.name) ? Parser::SUCCESS : Parser::CRITICAL_ERROR;
+		}
+	}
+	return Parser::CRITICAL_ERROR;
+}
+
 std::string ACommand::command_prefix_s::toString() const  {
 	std::string ret = name.empty() ? "" : ":" + name;
 	ret += user.empty() ? "" : "!" + user;
