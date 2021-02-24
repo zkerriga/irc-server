f79a0321050edb5edb99f950a56bd73b5d53f124
zkerriga <daniil.63@yandex.ru>
2021-02-24 15:18:15 +0300

    PONG full refactor

--- src/command/Pong.hpp
+++ src/command/Pong.hpp
@@ -15,39 +15,31 @@
 #include <string>
 
 #include "ACommand.hpp"
-#include "Parser.hpp"
-#include "ReplyList.hpp"
-#include "ServerInfo.hpp"
 
 class Pong : public ACommand {
 public:
-
 	static const char * const	commandName;
 
 	Pong(const std::string & commandLine, socket_type senderSocket, IServerForCmd & server);
 	virtual ~Pong();
 
-	static ACommand *			create(const std::string & commandLine, socket_type senderSocket, IServerForCmd & server);
-
+	static ACommand *			create(const std::string & commandLine,
+									   socket_type senderSocket, IServerForCmd & server);
 	virtual replies_container	execute(IServerForCmd & server);
 
 private:
-
-	bool	_isPrefixValid(const IServerForCmd & server);
-	bool	_isParamsValid(IServerForCmd & server);
-	void	_execute(IServerForCmd & server);
+	bool	_isParamsValid();
+	void	_execute();
 
 	static const Parser::parsing_unit_type<Pong> _parsers[];
 
-	Parser::parsing_result_type _targetParser(const std::string & targetArg);
-	Parser::parsing_result_type _tokenParser(const std::string & tokenArg);
+	Parser::parsing_result_type	_targetParser(const std::string & targetArg);
+	Parser::parsing_result_type	_tokenParser(const std::string & tokenArg);
 
-	std::string 	_target;
-	std::string 	_token;
+	std::string		_target;
+	std::string		_token;
 
 	Pong();
 	Pong(const Pong & other);
 	Pong & operator= (const Pong & other);
-
-
 };
