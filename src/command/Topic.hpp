205f9edd7755253ebbec080a0862b42e69d3982b
zkerriga <daniil.63@yandex.ru>
2021-02-24 16:14:57 +0300

    TOPIC full refactor

--- src/command/Topic.hpp
+++ src/command/Topic.hpp
@@ -26,20 +26,19 @@ public:
 	static std::string			createReply(const std::string & channel, const std::string & topic);
 	virtual replies_container	execute(IServerForCmd & server);
 	static ACommand *			create(const std::string & commandLine,
-										socket_type senderSocket, IServerForCmd & server);
+									   socket_type senderSocket, IServerForCmd & server);
 
 private:
 	Topic();
 	Topic(const Topic & other);
 	Topic & operator= (const Topic & other);
 
-	bool		_parsingIsPossible(const IServerForCmd & server);
-	void		_execute(IServerForCmd & server, const IClient * client);
-	void		_setTopic(const IServerForCmd & server, const std::string & topic);
-	void		_getTopic(const IServerForCmd & server);
+	bool	_parsingIsPossible();
+	void	_execute(const IClient * client);
+	void	_setTopic(const std::string & topic);
+	void	_getTopic();
 
 	static const Parser::parsing_unit_type<Topic>	_parsers[];
-	Parser::parsing_result_type	_commandNameParser(const std::string & commandArgument);
 	Parser::parsing_result_type	_channelParser(const std::string & channelArgument);
 	Parser::parsing_result_type	_topicParser(const std::string & topicArgument);
 
