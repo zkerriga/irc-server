0d5271d473d795121506c88fcc8af16d82dc7901
zkerriga <daniil.63@yandex.ru>
2021-02-24 13:11:27 +0300

    MOTD full refactor

--- src/command/Motd.cpp
+++ src/command/Motd.cpp
@@ -11,11 +11,14 @@
 /* ************************************************************************** */
 
 #include "Motd.hpp"
-#include "BigLogger.hpp"
-#include "IClient.hpp"
 #include <iostream>
 #include <fstream>
+
+#include "BigLogger.hpp"
+#include "IClient.hpp"
 #include "debug.hpp"
+#include "Wildcard.hpp"
+#include "ServerInfo.hpp"
 
 Motd::Motd() : ACommand("", "", 0, nullptr) {}
 Motd::Motd(const Motd & other) : ACommand("", "", 0, nullptr) {
@@ -38,30 +41,32 @@ ACommand * Motd::create(const std::string & commandLine,
 }
 
 const char * const	Motd::commandName = "MOTD";
+#define CMD std::string(commandName)
 
 ACommand::replies_container Motd::execute(IServerForCmd & server) {
-    BigLogger::cout(std::string(commandName) + ": execute");
-    if (_isParamsValid(server)) {
-        _execute(server);
-    }
-    return _commandsToSend;
+	if (_isParamsValid()) {
+		_execute();
+	}
+	return _commandsToSend;
 }
 
-void Motd::_execute(IServerForCmd & server) {
+void Motd::_execute() {
 	// check if we have target
 	if (!_target.empty()) {
-		DEBUG3(BigLogger::cout(std::string(commandName) + " : target provided, finding server: " + _target,
+		DEBUG3(BigLogger::cout(CMD + " : target provided, finding server: " + _target,
 							   BigLogger::YELLOW);)
-		if (Wildcard(_target) != server.getName()) {
-			DEBUG2(BigLogger::cout(std::string(commandName) + " : we are not match! finding target server...",
+		if (Wildcard(_target) != _server->getName()) {
+			DEBUG2(BigLogger::cout(CMD + " : we are not match! finding target _server->..",
 								   BigLogger::YELLOW);)
-			std::list<ServerInfo *> servList = server.getAllServerInfoForMask(_target);
+			std::list<ServerInfo *> servList = _server->getAllServerInfoForMask(_target);
 			if (servList.empty()) {
-				DEBUG3(BigLogger::cout(std::string(commandName) + " : server not found!", BigLogger::YELLOW);)
-				_addReplyToSender(server.getPrefix() + " " + errNoSuchServer(_prefix.name, _target));
+				DEBUG3(BigLogger::cout(CMD + " : server not found!", BigLogger::YELLOW);)
+				_addReplyToSender(
+					_server->getPrefix() + " " + errNoSuchServer(_prefix.name, _target)
+				);
 			}
 			else {
-				DEBUG3(BigLogger::cout(std::string(commandName) + " : server found, forwarding to " +
+				DEBUG3(BigLogger::cout(CMD + " : server found, forwarding to " +
 									   (*servList.begin())->getName(), BigLogger::YELLOW);)
 				// note: _createRawReply() works only with "MOTD target" format
 				_addReplyTo((*servList.begin())->getSocket(), _createRawReply());
@@ -69,29 +74,29 @@ void Motd::_execute(IServerForCmd & server) {
 			return;
 		}
 	}
-	_sendMotd(server);
+	_sendMotd();
 }
 
-void Motd::_sendMotd(IServerForCmd & server) {
-	DEBUG2(BigLogger::cout(std::string(commandName) + " : sending motd to " + _prefix.name, BigLogger::YELLOW);)
+void Motd::_sendMotd() {
+	DEBUG2(BigLogger::cout(CMD + " : sending motd to " + _prefix.name, BigLogger::YELLOW);)
 
-	std::string line;
-	std::string pathMotd = "./motd";
-	std::ifstream file(pathMotd);
-	DEBUG3(BigLogger::cout(std::string(commandName) + " : trying to open MOTD file: " + pathMotd, BigLogger::YELLOW);)
+	std::string		line;
+	std::string		pathMotd = "./motd";
+	std::ifstream	file(pathMotd);
+	DEBUG3(BigLogger::cout(CMD + " : trying to open MOTD file: " + pathMotd, BigLogger::YELLOW);)
 	if (file.is_open())
 	{
-		DEBUG3(BigLogger::cout(std::string(commandName) + " : open successful", BigLogger::YELLOW);)
-		_addReplyToSender(server.getPrefix() + " " + rplMotdStart(_prefix.name, server.getName()));
+		DEBUG3(BigLogger::cout(CMD + " : open successful", BigLogger::YELLOW);)
+		_addReplyToSender(_server->getPrefix() + " " + rplMotdStart(_prefix.name, _server->getName()));
 		while (getline(file, line)){
-			_addReplyToSender(server.getPrefix() + " " + rplMotd(_prefix.name, line));
+			_addReplyToSender(_server->getPrefix() + " " + rplMotd(_prefix.name, line));
 		}
-		_addReplyToSender(server.getPrefix() + " " + rplEndOfMotd(_prefix.name));
+		_addReplyToSender(_server->getPrefix() + " " + rplEndOfMotd(_prefix.name));
 	}
 	else{
 		//выводим ошибку открытия если файл не найден
-		DEBUG3(BigLogger::cout(std::string(commandName) + " : open failed", BigLogger::YELLOW);)
-		_addReplyToSender(server.getPrefix() + " " + errNoMotd(_prefix.name));
+		DEBUG3(BigLogger::cout(CMD + " : open failed", BigLogger::YELLOW);)
+		_addReplyToSender(_server->getPrefix() + " " + errNoMotd(_prefix.name));
 	}
 	file.close();
 }
@@ -105,8 +110,8 @@ std::string Motd::_createRawReply() {
 
 /// PARSING
 
-bool Motd::_isParamsValid(const IServerForCmd & server) {
-	return Parser::argumentsParser(server,
+bool Motd::_isParamsValid() {
+	return Parser::argumentsParser(*_server,
 								   Parser::splitArgs(_rawCmd),
 								   _parsers,
 								   this,
@@ -115,20 +120,14 @@ bool Motd::_isParamsValid(const IServerForCmd & server) {
 
 const Parser::parsing_unit_type<Motd> Motd::_parsers[] = {
 	{.parser = &Motd::_defaultPrefixParser, .required = false},
-	{.parser = &Motd::_commandNameParser, .required = true},
+	{.parser = &Motd::_defaultCommandNameParser, .required = true},
 	{.parser = &Motd::_targetParser, .required = false},
 	{.parser = nullptr, .required = false}
 };
-
-Parser::parsing_result_type
-Motd::_commandNameParser(const std::string & commandNameArg) {
-	if (Parser::toUpperCase(commandNameArg) != commandName) {
-		return Parser::CRITICAL_ERROR;
-	}
-	return Parser::SUCCESS;
-}
 
 Parser::parsing_result_type Motd::_targetParser(const std::string & targetArg) {
 	_target = targetArg;
 	return Parser::SUCCESS;
 }
+
+#undef CMD
