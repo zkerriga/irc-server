4e341836c35a8866980deaa9e199704d26ef4c2a
zkerriga <daniil.63@yandex.ru>
2021-02-24 16:28:05 +0300

    USER full refactor

--- src/command/UserCmd.hpp
+++ src/command/UserCmd.hpp
@@ -26,24 +26,23 @@ public:
 	static ACommand *			create(const std::string & commandLine, socket_type senderSocket, IServerForCmd & server);
 	virtual replies_container	execute(IServerForCmd & server);
 
-	static const size_t localConnectionHopCount = 0;
+	static const size_t		localConnectionHopCount = 0;
 
 private:
 	UserCmd();
 	UserCmd(const UserCmd & other);
 	UserCmd & operator= (const UserCmd & other);
 
-	bool	_isPrefixValid(const IServerForCmd & server);
-	bool	_isParamsValid(IServerForCmd & server);
-	void	_execute(IServerForCmd & server);
-	void	_executeForClient(IServerForCmd & server, IClient * client);
+	bool	_isPrefixValid();
+	bool	_isParamsValid();
+	void	_execute();
+	void	_executeForClient(IClient * client);
 
-	void		_createAllReply(const IServerForCmd & server, const std::string & reply);
-	std::string _createWelcomeMessage(const IServerForCmd & server, const IClient * client) const;
+	std::string _createWelcomeMessage(const IClient * client) const;
 
-	std::string _username;
-	std::string _host;
-	std::string _servername;
-	std::string _realName;
+	std::string		_username;
+	std::string		_host;
+	std::string		_servername;
+	std::string		_realName;
 };
 
